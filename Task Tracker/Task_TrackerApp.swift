2121189a897d699ba2cf7b328ad638bac42ea28c
VedantPatle0305 <vedantpatle594@gmail.com>
2025-10-07 14:31:05 +0530

    Initial Commit

---
+++ Task Tracker/Task_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_TrackerApp.swift
+//  Task Tracker
+//
+//  Created by Vedant Patle on 07/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Task_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
